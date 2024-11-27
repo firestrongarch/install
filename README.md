e2bb499df1e82bd3a6067f7f422919cb63eda5e1
fu <1766423721@qq.com>
2024-11-27 16:44:46 +0800

    README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # install
 一键安装
+
+# Uage
+添加交换空间,默认为16g
+``` 
+python swap.py
--- swap.py
+++ swap.py
@@ -2,7 +2,7 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.run('mkdir myswapfile',shell=True)
-    subprocess.run('sudo dd if=/dev/zero of=./myswapfile/swapfile bs=1G count=1',shell=True)
+    subprocess.run('sudo dd if=/dev/zero of=./myswapfile/swapfile bs=1G count=16',shell=True)
     subprocess.run('sudo chmod 600 ./myswapfile/swapfile',shell=True)
     subprocess.run('sudo mkswap ./myswapfile/swapfile',shell=True)
     subprocess.run('sudo swapon ./myswapfile/swapfile',shell=True)
