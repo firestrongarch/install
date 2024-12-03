0b7d4eedf88881a6bce6a8efed936c8ceb7bfcec
firearch <65007434+firestrongarch@users.noreply.github.com>
2024-12-03 14:49:15 +0800

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,9 +1,8 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.run('mkdir myswapfile',shell=True)
-    subprocess.run('sudo dd if=/dev/zero of=./myswapfile/swapfile bs=1G count=16',shell=True)
-    subprocess.run('sudo chmod 600 ./myswapfile/swapfile',shell=True)
-    subprocess.run('sudo mkswap ./myswapfile/swapfile',shell=True)
-    subprocess.run('sudo swapon ./myswapfile/swapfile',shell=True)
-    subprocess.run('echo "./myswapfile/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab',shell=True)
+    subprocess.run('sudo dd if=/dev/zero of=/opt/myswapfile/swapfile bs=1G count=16',shell=True)
+    subprocess.run('sudo chmod 600 /opt/myswapfile/swapfile',shell=True)
+    subprocess.run('sudo mkswap /opt/myswapfile/swapfile',shell=True)
+    subprocess.run('sudo swapon /opt/myswapfile/swapfile',shell=True)
+    subprocess.run('echo "/opt/myswapfile/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab',shell=True)
